0f1f73ba1a9d8efb32850576ebe9bce9d5501e4e
catmull@CATMULL0-W.ad.corp.google.com <catmull@CATMULL0-W.ad.corp.google.com>
2011-04-12 16:37:17 -0600

    add XmlLiteParser

---
+++ XmlLiteParser.cc
@@ -0,0 +1,89 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#include "Diadem/XmlLiteParser.h"
+#include "Diadem/Factory.h"
+#include "Diadem/Value.h"
+
+#include <xmllite.h>
+
+namespace {
+
+// XML node and attribute names will be plain ASCII, so it's safe to cast
+// WCHAR to char.
+Diadem::String StringFromWCHARs(WCHAR *w) {
+  char *s = new char[wcslen(w)];
+
+  for (int i = 0; w[i] != 0; ++i)
+    s[i] = w[i];
+  return String(s, Diadem::String::kAdoptBuffer);
+}
+
+}
+
+namespace Diadem {
+
+static Entity* ParseStream(CComPtr<IStream> &stream, const Factory &factory) {
+  CComPtr<IXmlReader> reader;
+
+  if (FAILED(CreateXmlReader(
+      __uuidof(IXmlReader), static_cast<void**>(&reader), NULL)))
+    return NULL;
+  if (FAILED(reader->SetInput(stream)))
+    return NULL;
+
+  XmlNodeType node_type;
+  FactorySession session(factory)
+
+  while (reader->Read(&node_type) == S_OK) {
+
+    switch (node_type) {
+      case XmlNodeType_Element: {
+          const WCHAR *node_name = NULL;
+
+          if (FAILED(reader->GetLocalName(&name, NULL)))
+            break;
+          if (FAILED(reader->MoveToFirstAttribute()))
+            break;
+
+          PropertyMap properties;
+
+          while (reader->MoveToNextAttribute() == S_OK) {
+            const WCHAR *name = NULL, *value = NULL;
+
+            if (FAILED(reader->GetLocalName(&name, NULL)))
+              continue;
+            if (FAILED(reader->GetValue(&value, NULL)))
+              continue;
+            properties.Insert(StringFromWCHARs(name), StringFromWCHARs(value))
+          }
+          session.BeginEntity(StringFromWCHARs(node_name), properties);
+        break;
+      }
+      case XmlNodeType_EndElement:
+        session.EndEntity();
+        break;
+    }
+  }
+}
+
+Entity* LibXMLParser::LoadEntityFromFile(const char *path) const {
+  CComPtr<IStream> pFileStream;
+
+  if (FAILED(SHCreateStreamOnFile(path, STGM_READ, &file_stream))
+    return NULL;
+  return ParseStream(pFileStream, factory_);
+}
+
+}  // namespace Diadem
---
+++ XmlLiteParser.h
@@ -0,0 +1,33 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+#include "Diadem/Factory.h"
+
+namespace Diadem {
+
+class Factory;
+
+class XmlLiteParser : public Parser {
+ public:
+  explicit XmlLiteParser(const Factory &factory) : factory_(factory) {}
+  virtual ~XmlLiteParser() {}
+
+  Entity* LoadEntityFromFile(const char *path) const;
+  Entity* LoadEntityFromData(const char *data) const;
+
+ protected:
+  const Factory &factory_;
+};
+
+}  // namespace Diadem
